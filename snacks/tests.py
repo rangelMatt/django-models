92d39b77a1608b4615d0924e90b440a587191306
Matt Rangel <mattrangel@gmail.com>
2022-05-24 17:51:20 -0700

    more tests were added

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,16 +1,30 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from .models import Snack
 
-# Create your tests here.
 
-class SnackTests(TestCase):
+class Snackstests(TestCase):
+  def setUp(self):
+        purchaser = get_user_model().objects.create(username="tester",password="tester")
+        Snack.objects.create(name="pizza", purchaser=purchaser)
+
+
   def test_list_page_status_code(self):
-    url = reverse("snack_list")
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, 200)
-    
-  def test_list_page_template(self):
-    url = reverse("snack_list")
-    response = self.client.get(url)
-    self.assertTemplateUsed(response, "snack_list.html")
-    self.assertTemplateUsed(response, "base.html")
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+  def test_detail_page_template(self):
+        url = reverse('snack_detail',args=(1,))
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_detail.html')
+
+  def test_list_page_context(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        snacks = response.context['object_list']
+        self.assertEqual(len(snacks), 1)
+        self.assertEqual(snacks[0].name, "pizza")       
+        self.assertEqual(snacks[0].purchaser.username, "tester")
