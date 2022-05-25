5787b25ce2fd9f2d0fefbd2e6ba5e9043fde7714
Matt Rangel <mattrangel@gmail.com>
2022-05-24 17:20:39 -0700

    finished lab 27

--- snacks/tests.py
+++ snacks/tests.py
@@ -10,4 +10,7 @@ class SnackTests(TestCase):
     self.assertEqual(response.status_code, 200)
     
   def test_list_page_template(self):
-    
+    url = reverse("snack_list")
+    response = self.client.get(url)
+    self.assertTemplateUsed(response, "snack_list.html")
+    self.assertTemplateUsed(response, "base.html")
--- snacks/views.py
+++ snacks/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Snack
+
 
 class SnackListView(ListView):
   template_name = 'snack_list.html'
+  model = Snack
+
+class SnackDetailView(DetailView):
+  template_name = 'snack_detail.html'
   model = Snack
